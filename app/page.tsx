69fd641572d37002bc6a0e24bf00dff78439b047
Faruke <faruke@bursement.com>
2023-07-07 15:13:10 +0600

    added pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
-
+import { AiOutlineEye } from "react-icons/ai";
+import { AiOutlineRight } from "react-icons/ai";
+import { AiOutlineLeft } from "react-icons/ai";
 export default function Home() {
   return (
     <main className="">
@@ -91,7 +93,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="grid grid-cols-3 gap-[20px]">
+        <div className="grid grid-cols-3 gap-[20px] mb-[50px]">
           <div className="h-[auto] shadow-md rounded-[20px] overflow-hidden ">
             <div className="h-[200px] bg-slate-300 ">use img</div>
             <div className="px-[7px] py-[7px]">
@@ -117,9 +119,93 @@ export default function Home() {
                     <small>Publish on 2023</small>
                   </div>
                 </div>
-                <div></div>
+                <div>
+                  <AiOutlineEye className="inline"></AiOutlineEye> <samp>123</samp>
+                </div>
               </div>
             </div>
+          </div>
+          <div className="h-[auto] shadow-md rounded-[20px] overflow-hidden ">
+            <div className="h-[200px] bg-slate-300 ">use img</div>
+            <div className="px-[7px] py-[7px]">
+              <div className="flex items-center">
+                <h1 className="text-green-600 text-[16px] mr-[8px]">Selling your hom</h1>
+                <span className="bg-slate-300 rounded-sm px-[3px] py-[1px] text-[10px]">
+                  catagory
+                </span>
+              </div>
+              <h1 className="text-[24px] mb-[4px] font-bold">10 Tips for first time Homebuyers</h1>
+              <small className="mb-[15px]">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, eos architecto
+                sed quasi maxime tempore unde alias quas ratione qui eligendi blanditiis omnis
+                quisquam quis obcaecati iure quam assumenda accusamus.
+              </small>
+              <div className="flex items-center justify-between mt-[20px]">
+                <div className="flex items-center ">
+                  <div className="mr-[5px]">
+                    <div className="w-[40px] h-[40px] rounded-[100px] bg-neutral-400"></div>
+                  </div>
+                  <div>
+                    <p>Rayn</p>
+                    <small>Publish on 2023</small>
+                  </div>
+                </div>
+                <div>
+                  <AiOutlineEye className="inline"></AiOutlineEye> <samp>123</samp>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="h-[auto] shadow-md rounded-[20px] overflow-hidden ">
+            <div className="h-[200px] bg-slate-300 ">use img</div>
+            <div className="px-[7px] py-[7px]">
+              <div className="flex items-center">
+                <h1 className="text-green-600 text-[16px] mr-[8px]">Selling your hom</h1>
+                <span className="bg-slate-300 rounded-sm px-[3px] py-[1px] text-[10px]">
+                  catagory
+                </span>
+              </div>
+              <h1 className="text-[24px] mb-[4px] font-bold">10 Tips for first time Homebuyers</h1>
+              <small className="mb-[15px]">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, eos architecto
+                sed quasi maxime tempore unde alias quas ratione qui eligendi blanditiis omnis
+                quisquam quis obcaecati iure quam assumenda accusamus.
+              </small>
+              <div className="flex items-center justify-between mt-[20px]">
+                <div className="flex items-center ">
+                  <div className="mr-[5px]">
+                    <div className="w-[40px] h-[40px] rounded-[100px] bg-neutral-400"></div>
+                  </div>
+                  <div>
+                    <p>Rayn</p>
+                    <small>Publish on 2023</small>
+                  </div>
+                </div>
+                <div>
+                  <AiOutlineEye className="inline"></AiOutlineEye> <samp>123</samp>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-center">
+          <div className="flex flex-row text-white">
+            <button className="text-black">
+              <AiOutlineLeft></AiOutlineLeft>
+            </button>
+            <button className="w-[20px] h-[20px] rounded-[100px] bg-green-700 flex justify-center items-center text-[12px] mx-[8px]">
+              1
+            </button>
+            <button className="w-[20px] h-[20px] rounded-[100px] bg-green-700 flex justify-center items-center text-[12px]  mr-[3px]">
+              2
+            </button>
+            <button className="w-[20px] h-[20px] rounded-[100px] bg-green-700 flex justify-center items-center text-[12px]  mx-[8px]">
+              3
+            </button>
+            <button className="text-black">
+              <AiOutlineRight></AiOutlineRight>
+            </button>
           </div>
         </div>
       </div>
